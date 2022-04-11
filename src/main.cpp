3a070c5a268e12219cd045775fc149b4e30ab830
Noc <t.gelotte@gmail.com>
2022-04-11 22:50:16 +0200

    Cleaned up the pulled code

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,11 @@ Av: Timjan & Noc
 #include <Wire.h>
 #include <math.h>
 
-#include "test.h"
 #include "webCode.h"
-#include "UdpTransceiver.h"
 #include "MotorController.h"
-<<<<<<< HEAD
-//#include "WebServer.h"
-=======
 #include "SteeringController.h"
-#include "WebServer.h"
->>>>>>> 5b07937 (A lot things :O A bunch of motor and PID stuff)
 #include "auth.h"
+#include "UdpTransceiver.h"
 
 
 // --- Wifi and webserver stuff ---
@@ -42,7 +36,6 @@ MotorController steeringMotor(200, 13, 12);
 MotorController driveMotor(255, 14, 15);
 SteeringController steeringController(mpuUnit);
 
-//WebServer web(ssid, password, ssidAP, passwordAP);
 UdpTransceiver tcp(ssid, password, ssidAP, passwordAP);
 
 const int led = 3;
@@ -50,8 +43,8 @@ const int analogInPin = A0;
 
 const int buzzer = 2;
 
-
-//float calculateZGyroBias();
+int driveSpeed = 0;
+int targetDirection = 0;
 
 
 void setup() {
@@ -61,6 +54,7 @@ void setup() {
   Serial.println("Start of program!");
   tcp.initServer();
 
+  //web.initWebSocket();
   steeringMotor.init();
   driveMotor.init();
   steeringController.init();
@@ -73,27 +67,6 @@ void setup() {
 }
 
 
-  void customTone(uint8_t _pin, unsigned int frequency, unsigned long duration) {
-    //pinMode (_pin, OUTPUT );
-    analogWriteFreq(frequency);
-    analogWrite(_pin, 50);
-    delay(duration);
-    analogWrite(_pin,0);
-  }
-
-  void engineSound(unsigned int motorSpeed) {
-    int maxFrequency = 400;
-    int minFrequency = 30;
-
-    int PWM = 150;
-
-    int frequency = map(motorSpeed, 0, 100, minFrequency, maxFrequency);
-
-    analogWriteFreq(frequency);
-    analogWrite(buzzer, PWM);
-
-  }
-
   void honk(bool honkOn) {
     if (honkOn) {
       analogWriteFreq(800);
@@ -102,132 +75,55 @@ void setup() {
     else
       analogWrite(buzzer, 0);
   }
-/*
-  void timjanTone (uint8_t pin, unsigned int freq, unsigned int duration) {
-    unsigned int startTime = micros();
-    while(true) {
-      analogWrite(pin, sin(micros()*freq*1000000)*255);
-      if (micros() > startTime + duration) {
-        analogWrite(pin, 0);
-        break;
-      }
-    }
-    
-  }
-*/
+
+
 void loop() {
-<<<<<<< HEAD
-  delay(10);
+  
   tcp.update();
+
+  // --- Fetching and calculating signals ---------------------------------
+  
   uint8_t packet[255];
   tcp.receiveData(packet);
 
+  if (packet[0] == TARGET_SPEED_HEADER) {
+    driveSpeed = packet[1];
+  }
 
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  tcp.publishSensor(GYROSCOPE_HEADER,g.data,micros());
-=======
-  
-  web.updateWebSocket();
- 
-
- // --- Fetching and calculating signals ---------------------------------
-  
-  int x = web.getX();
-  int y = web.getY();
+  if (packet[0] == STICK_DIRECTION_HEADER) {
+    targetDirection = packet[1];
+  }
   
   steeringController.readMPUValues();
   float z = steeringController.zGyro;
-  float controlSignal = steeringController.PIDController(float(x), z);
+  float steeringControlSignal = steeringController.PIDController(float(targetDirection), z);
 
 
   // --- Code for motors --------------------------------------------------- 
 
-  int motorSpeed = map(abs(y), 0, 200, 0, 100);
 
-  if (y < 0) {
-    driveMotor.drive(MotorController::forward, motorSpeed);
-    steeringMotor.autoSteer(controlSignal, MotorController::forward);
+  if (driveSpeed > 0) {
+    driveMotor.drive(MotorController::forward, driveSpeed);
+    steeringMotor.autoSteer(steeringControlSignal, MotorController::forward);
   }
-  else if (y > 0) {
-    driveMotor.drive(MotorController::reverse, motorSpeed);
-    steeringMotor.autoSteer(controlSignal, MotorController::reverse);
+  else if (driveSpeed < 0) {
+    driveMotor.drive(MotorController::reverse, driveSpeed);
+    steeringMotor.autoSteer(steeringControlSignal, MotorController::reverse);
   }
   else {
     driveMotor.drive(MotorController::neutral, 0);
-    steeringMotor.autoSteer(controlSignal, MotorController::neutral);
+    steeringMotor.autoSteer(steeringControlSignal, MotorController::neutral);
     steeringController.calculateZGyroBias();
   }
 
 
   // --- LED and other stuff -------------------------------------
 
-  if (y > 10)
+  if (driveSpeed < -10)
     digitalWrite(led, HIGH);
   else
     digitalWrite(led, LOW);
 
-  //engineSound(motorSpeed);
 
-  if (motorSpeed != 0) 
-    honk(true);
-  
-  else
-    honk(false);
-  
-/*
-   for (int i = 50; i < 200; i++) {
-     //tone(buzzer, i);
-     customTone(buzzer, i, 50);
-     //timjanTone(buzzer, 400, 50);
-     //delay(50);
-   }
-   delay(1000);
-   for (int i = 200; i > 50; i--) {
-     //tone(buzzer, i);
-     customTone(buzzer, i, 50);
-     //delay(50);
-   }
-*/
-  // --- Debug shit ------------------------------------------------
-/*
-  Serial.print("Z: ");
-  Serial.print(abs(z));
-  Serial.print("  PID: ");
-  Serial.print(controlSignal);
-*/
-  Serial.println(steeringController.zGyro);
-  //Serial.println("");
-
-  /*
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.print(" m/s^2    ");
-
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-*/
->>>>>>> 5b07937 (A lot things :O A bunch of motor and PID stuff)
-
-  /*
-  Serial.print("Angle:");
-  Serial.print(web.getAngle());
-  Serial.print("   Speed:");
-  Serial.print(web.getSpeed());
-  Serial.print("   x:");
-  Serial.print(web.getX());
-  Serial.print("   y:");
-  Serial.println(web.getY());
-*/
- 
   delay(20);
 }
